62452b4279be51d6fe591ff8e1cd485720b89572
SosGulunov <99356067+SosGulunov@users.noreply.github.com>
2022-12-17 00:12:04 +0300

    update1

--- firstapp/models.py
+++ firstapp/models.py
@@ -7,3 +7,21 @@ class Person(models.Model):
     objects = models.Manager()
     DoesNotExist = models.Manager
 
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    course = models.ManyToManyField(Course)
+
+
+class Product(models.Model):
+    company = models.ForeignKey(Company,on_delete= models.CASCADE)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
